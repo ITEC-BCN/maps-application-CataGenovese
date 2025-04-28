1c377969e0d9acb72ec2b46394216c1abe2815aa
Catalina <catalina.genovese.7e8@itb.cat>
2025-04-28 19:43:16 +0200

    metodes crud fets

--- app/src/main/java/com/example/mapsapp/data/MySupabaseClient.kt
+++ app/src/main/java/com/example/mapsapp/data/MySupabaseClient.kt
@@ -1,4 +1,66 @@
-package com.example.mapsapp.data
+package com.example.mapsapp.ui.navigation
 
-class MySupabaseClient {
+import com.example.mapsapp.data.Marker_bdd
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.postgrest.from
+
+class MySupabaseClient() {
+    lateinit var client: SupabaseClient
+
+    constructor(supabaseUrl: String, supabaseKey: String) : this() {
+        client = createSupabaseClient(
+            supabaseUrl = supabaseUrl,
+            supabaseKey = supabaseKey
+        ) {
+            install(Postgrest)
+        }
+    }
+
+    //SQL operations
+    suspend fun getAllMarkers(): List<Marker_bdd> {
+        return client.from("Map").select().decodeList<Marker_bdd>()
+    }
+
+    suspend fun getMarker(id: Int): Marker_bdd {
+        return client.from("Map").select {
+            filter {
+                eq("id", id)
+            }
+        }.decodeSingle<Marker_bdd>()
+    }
+
+    suspend fun insertMarker(marker: Marker_bdd) {
+        client.from("Map").insert(marker)
+    }
+
+    suspend fun updateMarker(
+        id: Int,
+        name: String,
+        description: String,
+        coordenades: String,
+        foto: String
+    ) {
+        client.from("Map").update({
+            set("name", name)
+            set("description", description)
+            set("description", description)
+            set("coordenades", coordenades)
+            set("foto", foto)
+        }) {
+            filter {
+                eq("id", id)
+            }
+        }
+    }
+
+    suspend fun deleteMarker(id: String) {
+        client.from("Map").delete {
+            filter {
+                eq("id", id)
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/mapsapp/viewmodels/ViewModelApp.kt
+++ app/src/main/java/com/example/mapsapp/viewmodels/ViewModelApp.kt
@@ -3,33 +3,102 @@ package com.example.mapsapp.viewmodels
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.mapsapp.data.Marker_bdd
+import com.example.mapsapp.ui.navigation.MySupabaseClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class ViewModelApp: ViewModel() {
+class ViewModelApp : ViewModel() {
 
     val database = MyApp.database
 
     //nom del lloc on farem la foto
-    private val _namePlace= MutableLiveData<String>()
-    val namePlace= _namePlace
+    private val _namePlace = MutableLiveData<String>()
+    val namePlace = _namePlace
 
     //descripcio
     private val _description = MutableLiveData<String>()
     val description = _description
 
-    //funcio per modificar el nom
+    //markerActual
+    private var _actualMarker: Marker_bdd? = null
 
+    //lista marcadores
+    private val _markersList = MutableLiveData<List<Marker_bdd>>()
+    val markerList = _markersList
 
     //coordenades
-    private val coordenades=0
+    private val coordenades = MutableLiveData<Long>()
 
-    /*   === SETTERS ===   */
 
-    fun setName(name:String) {
-        _namePlace.value= name
+    /*========SETTERS========*/
+
+    fun setName(name: String) {
+        _namePlace.value = name
     }
 
     fun setDescription(novaDescription: String) {
-        _description.value= novaDescription
+        _description.value = novaDescription
+    }
+
+    /*CRUD*/
+
+    fun getAllMarkers() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val databaseMarkers = database.getAllMarkers()
+            withContext(Dispatchers.Main) {
+                _markersList.value = databaseMarkers
+            }
+        }
+    }
+
+    fun insertNewMarker(name: String, description: String, coordenades: String, foto: String) {
+        val newMarker = Marker_bdd(
+            id = 0,
+            name = name,
+            description = description,
+            coordenades = coordenades,
+            foto = foto
+        )
+        CoroutineScope(Dispatchers.IO).launch {
+            database.insertMarker(newMarker)
+            getAllMarkers()
+        }
+    }
+
+    fun updateMarker(
+        id: Int,
+        name: String,
+        description: String,
+        coordenades: String,
+        foto: String
+    ) {
+        CoroutineScope(Dispatchers.IO).launch {
+            database.updateMarker(id, name, description, coordenades, foto)
+            getAllMarkers()
+        }
+    }
+
+    fun deleteMarker(id: Int) {
+        CoroutineScope(Dispatchers.IO).launch {
+            database.deleteMarker(id.toString())
+            getAllMarkers()
+        }
+    }
+
+    fun getMarker(id: Int) {
+        if (_actualMarker == null) {
+            CoroutineScope(Dispatchers.IO).launch {
+                val marker = database.getMarker(id)
+                withContext(Dispatchers.Main) {
+                    _actualMarker = marker
+                    _namePlace.value = marker.name
+                    _description.value = marker.description
+                }
+            }
+        }
     }
 
 }
