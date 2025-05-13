3a51f0c3c82e3f5fcf5bd5a3ca699fa2e922c1b4
catastrófica <catalina.genovese.7e8@itb.cat>
2025-05-13 13:51:12 +0200

    screen login/ screen register / deleteimg

--- app/src/main/java/com/example/mapsapp/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/mapsapp/ui/screens/LoginScreen.kt
@@ -1,9 +1,17 @@
 package com.example.mapsapp.ui.screens
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mapsapp.utils.SharedPreferencesHelper
+import com.example.mapsapp.viewmodels.AuthViewModel
+import com.example.mapsapp.viewmodels.ViewModelApp
+import com.example.mapsapp.viewmodels.ViewModelFactory
 
 @Composable
     //navTOdRAWER
 fun Login(navToRegister: () -> Unit, navToHome: () -> Unit){
+    val context= LocalContext.current
+    val viewModel: AuthViewModel = viewModel(factory = ViewModelFactory(SharedPreferencesHelper(context)))
 
 }
